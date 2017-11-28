a3d6fb96775cea528a3fcb9dccc3579842a22b90
Tahir Qayyum <qayyumtahir00@gmail.com>
2017-11-27 20:41:37 -0500

    commiting the file

---
+++ week_4/monday/app.js
@@ -0,0 +1,18 @@
+function function_name(argument) {
+	// body...
+	console.log("Hello");
+}
+
+
+var statement = 'HELLO WORLD';
+
+console.log(statement.toUpperCase());
+console.log(statement.toLowerCase());
+console.log(statement.slice(6,11));
+console.log(statement.replace('HELLO WORLD', 'TACOS'));
+
+
+console.log(Math.round(3.2));
+console.log(Math.round(3.5));
+console.log(Math.floor(3.5));
+console.log(Math.ceil(3.5));
