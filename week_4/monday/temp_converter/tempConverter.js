afcefe35c3cca449cf6d512814c35f7a00c66102
Tahir Qayyum <qayyumtahir00@gmail.com>
2017-11-28 14:13:59 -0500

    Changed alert to console.log

--- week_4/monday/temp_converter/tempConverter.js
+++ week_4/monday/temp_converter/tempConverter.js
@@ -2,7 +2,7 @@ var temp = prompt("Enter the value :");
 var scale = prompt("Select a scale F to C or C to F:").toLowerCase();
 
 if (scale === 'f to c') {
-	alert((temp - 32)*.5556);
+	console.log((temp - 32)*.5556);
 }else{ (scale === 'c to f')
-	alert((temp * 1.8)+32);
+	console.log((temp * 1.8)+32);
 }
