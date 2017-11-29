599663ee82a2fff9211fa30ccef4f02efc029382
Tahir Qayyum <qayyumtahir00@gmail.com>
2017-11-28 21:30:42 -0500

    Adding the calculator lab

---
+++ week_4/monday/calculator/calculator.js
@@ -0,0 +1,22 @@
+var val1 = prompt("Enter the 1st value: ");//.parseInt();
+var val2 = prompt("Enter the 2nd value: ");//.parseInt();
+var symbol = prompt("Enter one of the mathematical operators +, -, *, / or ^ : ");
+var total = parseInt(val1) + parseInt(val2);
+
+if (symbol === '+') {
+	alert(val1 += val2);
+}
+/*
+if (symbol === '-') {
+	alert(val1 -= val2);
+}
+if (symbol === '*') {
+	alert(val1 *= val2);
+}
+if (symbol === '/') {
+	alert(val1 /= val2);
+}
+if (symbol === '^') {
+	alert((val1 ^ val2));
+}
+*/
