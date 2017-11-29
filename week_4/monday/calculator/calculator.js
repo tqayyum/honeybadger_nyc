7909e6e2d52386aa1cf6fa575cb559ccb781660a
Tahir Qayyum <qayyumtahir00@gmail.com>
2017-11-29 12:57:41 -0500

    Add function works with parseInt()

--- week_4/monday/calculator/calculator.js
+++ week_4/monday/calculator/calculator.js
@@ -1,12 +1,10 @@
-var val1 = prompt("Enter the 1st value: ");//.parseInt();
-var val2 = prompt("Enter the 2nd value: ");//.parseInt();
+var val1 = parseInt(prompt("Enter the 1st value: "));
+var val2 = parseInt(prompt("Enter the 2nd value: "));
 var symbol = prompt("Enter one of the mathematical operators +, -, *, / or ^ : ");
-var total = parseInt(val1) + parseInt(val2);
 
 if (symbol === '+') {
 	alert(val1 += val2);
 }
-
 if (symbol === '-') {
 	alert(val1 -= val2);
 }
