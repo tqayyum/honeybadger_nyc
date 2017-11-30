bde3b656de6653aa3bb88c7702519b5e065df011
Tahir Qayyum <qayyumtahir00@gmail.com>
2017-11-29 19:35:50 -0500

    Saving all the files

--- week_4/monday/calculator/calculator.js
+++ week_4/monday/calculator/calculator.js
@@ -1,18 +1,20 @@
-var val1 = parseInt(prompt("Enter the 1st value: "));
-var val2 = parseInt(prompt("Enter the 2nd value: "));
-var symbol = prompt("Enter one of the mathematical operators +, -, *, / or ^ : ");
+const val1 = parseInt(prompt("Enter the 1st value: ")) || 0;
+const val2 = parseInt(prompt("Enter the 2nd value: ")) || 0;
+const symbol = prompt("Enter one of the mathematical operators +, -, *, / or ^ : ");
 
+
+// This section is were the operation is done
 if (symbol === '+') {
-	alert(val1 += val2);
+	alert(val1 + val2);
 }
 if (symbol === '-') {
-	alert(val1 -= val2);
+	alert(val1 - val2);
 }
 if (symbol === '*') {
-	alert(val1 *= val2);
+	alert(val1 * val2);
 }
-if (symbol === '/') {
-	alert(val1 /= val2);
+if (symbol === '/' && val2 !== 0) {
+	alert(val1 / val2);
 }
 if (symbol === '^') {
 	alert(Math.pow(val1, val2));
