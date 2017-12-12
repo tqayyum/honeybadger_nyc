e24f2a97840ef3816c2799d0ce65fd66ed4974ef
Tahir Qayyum <qayyumtahir00@gmail.com>
2017-12-12 09:15:37 -0500

    Finally

--- week_6/monday/cw/pixels/js/app.js
+++ week_6/monday/cw/pixels/js/app.js
@@ -1,11 +1,9 @@
 // The window.onload function runs when the document is loaded
 window.onload = function() {
   console.log("Loaded, bro.");
-
-	const gridc = document.getElementById('container');
-	const newDiv = document.createElement('div');
-			newDiv.classList.add('gridColumn');
-			//newDiv.innerHTML = 'This the fourth Li';
-			//gridc.appendChild(newDiv);
-			newDiv.classList.add ('pixel');
+	
+	const 	grid = document.getElementById('container');
+	const 	newDiv = document.createElement('div');
+			newDiv.classList.add('pixel')
+			grid.appendChild(newDiv);
 };
