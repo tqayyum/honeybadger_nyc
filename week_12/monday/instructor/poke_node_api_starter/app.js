3c0598a4a767666e1c25f1ed29b1e5ec35929b7b
Tahir Qayyum <qayyumtahir00@gmail.com>
2018-01-24 09:43:36 -0500

    added back week_12

---
+++ week_12/monday/instructor/poke_node_api_starter/app.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const logger = require('morgan');
+const app = express();
+const pokemon = require('./data/poke_array');
+
+app.use(logger('dev'));
+
+// INDEX ROUTE FOR THE POKEMON SHOULD SEND ALL THE POKEMON TO THE USER
+app.get('/pokemons', function (req, res) {
+    res.send(pokemon);
+});
+
+// BE ABLE TO SEARCH FOR A POKEMON BY NAME
+app.get('/pokemons/byName/:name', function (req, res) {
+    const name = req.params.name;
+    const result = pokemon.find(function(poke) {
+        return poke.name.toLowerCase() === name;
+    })
+
+    res.send(result);
+});
+
+// BE ABLE TO SEARCH FOR A POKEMON BY TYPE
+// '/pokemons/byType/:type'
+app.get('/pokemons/byType/:type', function (req, res) {
+    const type = req.params.type;
+    const result = pokemon.filter(function(poke) {
+        return poke.type.includes(type);
+    })
+
+    res.send(result);
+});
+// // BE ABLE TO GET A RANDOM POKEMON
+//
+app.get('/pokemons/random', function(req, res) {
+    const index = Math.floor(Math.random() * pokemon.length);
+    res.send(pokemon[index]);
+});
+
+// BE ABLE TO SEARCH FOR A POKEMON BY ID
+//
+app.get('/pokemons/:id', function (req, res) {
+    const id = req.params.id;
+    const result = pokemon.find(function(poke) {
+        return poke.id === id;
+    });
+
+    res.send(result);
+});
+
+app.listen(3000);
