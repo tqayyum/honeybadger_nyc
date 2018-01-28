91dc5e59efbd65f2275a1a1a34de3cba205f86ce
Tahir Qayyum <qayyumtahir00@gmail.com>
2018-01-28 13:58:24 -0500

    updating

--- hw/practice_2/app.js
+++ hw/practice_2/app.js
@@ -1,31 +1,85 @@
 const express = require('express');
 const logger = require('morgan');
+const request = require('request-promise');
+const exphbs  = require('express-handlebars');
+const favicon = require('serve-favicon');
+const path = require('path');
+const { clientId, clientSecret }= require('./api_key');
 const app = express();
 
-app.use	(express.static
-		(path.join
-		(__dirname, 
-		('pubic'))));
+app.use(logger('dev'));
+app.engine('handlebars', exphbs({defaultLayout: 'index'}));
+app.set('view engine', 'handlebars');
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
-app.get('/index.html', function(req, res) {
-	res.sendFile(_dirname + '/' + 'index.html');
-	console.log(_dirname + '/' + 'index.html');
+var authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    method: 'POST',
+    headers: {
+        'Authorization': 'Basic ' + (new Buffer(`${clientId}:${clientSecret}`).toString('base64'))
+    },
+    form: {
+        grant_type: 'client_credentials'
+    },
+    json: true
+};
+
+function getAccessToken() {
+    return request(authOptions).then(function(data) {
+       return data.access_token;
+    })
+}
+
+function getRequestOptions(url, access_token, queryOptions = {}) {
+    var options = {
+        url: url,
+        qs: queryOptions,
+        headers: {
+            'Authorization': `Bearer ${access_token}`
+        },
+        json: true
+    };
+
+    return options;
+}
+
+function getArtistByName(access_token, artist) {
+    var options = getRequestOptions('https://api.spotify.com/v1/search', access_token, { q: artist, type: 'artist' })
+
+    return request(options).then(function(artistData) {
+        return artistData;
+    });
+}
+
+function getTopTracks(access_token, id) {
+    var options = getRequestOptions(`https://api.spotify.com/v1/artists/${id}/top-tracks`, access_token, { country: 'US'});
+
+    return request(options).then(function(topTracks) {
+        return topTracks;
+    })
+}
+
+app.get('/:artist', function(req, res) {
+    const artist = req.params.artist;
+    let _access_token;
+
+    getAccessToken()
+        .then(function(access_token) {
+            _access_token = access_token;
+
+            return getArtistByName(_access_token, artist)
+        })
+        .then(function(data) {
+            const artist = data.artists.items[0] || {};
+            const id = artist.id;
+
+            return getTopTracks(_access_token, id);
+        })
+        .then(function(topTracks) {
+            res.render('artists');
+        })
 });
 
-app.get('/', function(req, res) {
-	res.send('This is the home page.');
-});
-
-app.get('/process', function(req, res) {
-	//Prepare output in JSON format
-	response = {
-		first_name:req.query.first_name,
-		last_name:req.query.last_name
-	};
-	console.log(response);
-	res.end(JSON.stringify(response));
-});
-
-app.listen(8080, function() {
-	console.log('Are you listening to port 8080');
+app.listen(3000, function() {
+    console.log('Listening on port 3000');
 });
--- hw/spotify_player/app.js
+++ hw/spotify_player/app.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const logger = require('morgan');
 const request = require('request-promise');
+const exphbs  = require('express-handlebars');
+const favicon = require('serve-favicon');
+const path = require('path');
 const { clientId, clientSecret }= require('./api_key');
 const app = express();
 
 app.use(logger('dev'));
+app.engine('handlebars', exphbs({defaultLayout: 'index'}));
+app.set('view engine', 'handlebars');
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
 var authOptions = {
     url: 'https://accounts.spotify.com/api/token',
@@ -52,6 +58,23 @@ function getTopTracks(access_token, id) {
         return topTracks;
     })
 }
+function normalizeTrackData(track) {
+    const {
+        album: {
+            name: albumName
+        },
+        name: trackName,
+        artists: [{
+            name: artistName
+        }]
+    } = track;
+
+    return {
+        albumName,
+        trackName,
+        artistName
+    }
+}
 
 app.get('/:artist', function(req, res) {
     const artist = req.params.artist;
@@ -64,12 +87,21 @@ app.get('/:artist', function(req, res) {
             return getArtistByName(_access_token, artist)
         })
         .then(function(data) {
-            const id = data.artists.items[0].id;
+            const artist = data.artists.items[0] || {};
+            const id = artist.id;
 
             return getTopTracks(_access_token, id);
         })
         .then(function(topTracks) {
-            res.send(topTracks);
+            const normalizeTrackData = topTracks.tracks.map(function(track) {
+                return normalizeTrackData(track);
+            })
+
+            const result = {
+            }
+        }
+            //res.send(topTracks);
+            //res.render('artists');
         })
 });
 
